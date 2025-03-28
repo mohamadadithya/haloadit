2e8376363ae79d7ab1a16831ea2ac95772ff2e8d
mohamadadithya <mohamadadithya@gmail.com>
2025-03-28 13:17:16 +0700

    add some required deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import vercel from '@astrojs/vercel';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    svelte(),
+    mdx(),
+    sitemap(),
+    sentry(),
+    spotlightjs()
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: vercel()
+});

8b83581287c87c9df4da41b223cfda37ddbd5f6b
mohamadadithya <mohamadadithya@gmail.com>
2025-04-14 08:31:04 +0700

    refactor(app): update: rss data integration

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,25 +1,39 @@
 import rss from "@astrojs/rss";
+import { contentfulClient, type BlogPost } from "@contentful";
 import siteConfig from "@site-config";
 import type { AstroConfig } from "astro";
-import { getCollection } from "astro:content";
 
 export async function GET({ site }: AstroConfig) {
-  const posts = await getCollection("posts");
+  const entries = await contentfulClient.getEntries<BlogPost>({
+    content_type: "blogPost",
+  });
+
+  const posts = entries.items.map((item) => {
+    const {
+      fields: { title, date, description, slug },
+      metadata: { tags },
+    } = item;
+
+    return {
+      title,
+      slug,
+      description,
+      tags,
+      date,
+    };
+  });
 
   return rss({
     title: `${siteConfig.title} - RSS Feed`,
     description: siteConfig.description,
     site: site || siteConfig.origin,
     trailingSlash: false,
-    items: posts.map((post) => {
-      const { data } = post;
-
-      return {
-        ...data,
-        link: `/${data.slug || post.id}`,
-        customData: `<language>id-ID</language>`,
-      };
-    }),
+    items: posts.map((post) => ({
+      ...post,
+      pubDate: new Date(post.date),
+      link: `/${post.slug}`,
+      customData: `<language>id-ID</language>`,
+    })),
     customData: `<language>id-ID</language>`,
     stylesheet: "/styles/rss.xsl",
   });

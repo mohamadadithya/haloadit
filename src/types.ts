d6b4d68d22518c4e888a51100eb5a25e31a5844c
mohamadadithya <mohamadadithya@gmail.com>
2025-04-15 14:25:34 +0700

    refactor(app): update: article list component structure

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,20 @@
+import type { TagLink } from "contentful";
+
 interface TOCItem {
   id: string;
   text: string;
   level: "h2" | "h3" | "h4" | "h5" | "h6";
 }
 
-export type { TOCItem };
+interface PostListItem {
+  title: string;
+  slug: string;
+  description: string;
+  tags: {
+    sys: TagLink;
+  }[];
+  date: string;
+}
+[];
+
+export type { TOCItem, PostListItem };

a3448426a5b6cbbc3f710663c66f6b4a01a55862
mohamadadithya <mohamadadithya@gmail.com>
2025-04-17 10:03:12 +0700

    refactor(app): update TOC and image max width

--- src/components/article/TOC.svelte
+++ src/components/article/TOC.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { TOCItem } from "src/types";
-  import { onMount } from "svelte";
   import { fly } from "svelte/transition";
 
   interface Props {
@@ -8,29 +7,6 @@
   }
 
   let { items }: Props = $props();
-  let activeId = $state<TOCItem["id"]>();
-
-  onMount(() => {
-    const headings = items
-      .map((item) => document.getElementById(item.id))
-      .filter(Boolean);
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) activeId = entry.target.id;
-        });
-      },
-      {
-        rootMargin: "0px 0px -70% 0px", // Trigger when heading hits 30% from top
-        threshold: 1.0,
-      }
-    );
-
-    headings.forEach((h) => observer.observe(h as HTMLElement));
-
-    return () => observer.disconnect();
-  });
 </script>
 
 <div
@@ -38,13 +14,10 @@
   class="toc flex-1 lg:sticky lg:top-24 p-5 bg-base-200 w-full rounded-2xl shadow"
 >
   <p class="font-lora font-semibold mb-4 text-primary uppercase">Daftar Isi</p>
-  <ul class="list-inside list-disc space-y-1.5">
+  <ul class="space-y-3 text-sm">
     {#each items as { id, text }}
-      {@const isActive = activeId === id}
       <li>
-        <a href={`#${id}`} class="link link-hover" class:link-primary={isActive}
-          >{text}</a
-        >
+        <a href={`#${id}`} class="link link-hover">{text}</a>
       </li>
     {/each}
   </ul>

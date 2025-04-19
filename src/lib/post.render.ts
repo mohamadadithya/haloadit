b5c9f0364ad4e1f01278e1fe0a6e501e3b934348
mohamadadithya <mohamadadithya@gmail.com>
2025-04-19 23:17:53 +0700

    refactor(app): update code block copy button

--- src/lib/post.render.ts
+++ src/lib/post.render.ts
@@ -92,13 +92,12 @@ export function createShikiCodeBlock(html: string): string {
         .trim();
 
       return `
-          <div class="code-block-wrapper relative group">
+          <div class="code-block-wrapper relative">
             <button
               type="button"
-              class="btn btn-sm btn-soft absolute top-2 right-2 opacity-0 group-hover:opacity-100 transition code-block-wrapper__copy-button"
-              data-clipboard-code="${plainText}"
-              >
-              Salin
+              class="btn btn-sm btn-soft absolute btn-square top-2 right-2 code-block-wrapper__copy-button"
+              data-clipboard-code="${plainText}" aria-label="Copy code">
+              <svg xmlns="http://www.w3.org/2000/svg" class="size-4" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></g></svg>
             </button>
             <pre class="shiki${classes}">${content}</pre>
           </div>

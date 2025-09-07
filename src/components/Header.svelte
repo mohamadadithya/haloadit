a7ffae82ea4abd1225c968778824cf1d16e88857
mohamadadithya <mohamadadithya@gmail.com>
2025-09-07 20:53:17 +0700

    refactor(app): remove settings button

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -33,18 +33,15 @@
   class="py-5 sticky top-0 z-50 duration-500 bg-base-100"
 >
   <Container
-    class="flex sm:items-center justify-between flex-col gap-5 sm:flex-row"
+    class="flex items-center justify-between flex-col gap-5 sm:flex-row"
   >
-    <div class="flex items-center justify-between">
-      <a href="/">
-        <p class="text-2xl heading-font">Halo Adit 👋</p>
-        <p class="text-xs">by {siteConfig.author}</p>
-      </a>
-      <HeaderActions class="sm:hidden" />
-    </div>
-    <div class="flex items-center gap-5">
+    <a href="/">
+      <p class="text-2xl heading-font">Halo Adit 👋</p>
+      <p class="text-xs">by {siteConfig.author}</p>
+    </a>
+    <div class="flex items-center gap-5 w-full sm:w-auto">
       <nav
-        class="border-t pt-5 sm:border-0 sm:pt-0 border-dashed border-primary w-full sm:border-r sm:pr-5 sm:border-primary"
+        class="border-t pt-5 sm:border-0 sm:pt-0 border-dashed border-primary w-full"
       >
         <ul class="flex justify-center items-center gap-8">
           {#each mainLinks as { label, href, isExternal }}
@@ -60,7 +57,6 @@
           {/each}
         </ul>
       </nav>
-      <HeaderActions class="hidden sm:flex" />
     </div>
   </Container>
 </header>
